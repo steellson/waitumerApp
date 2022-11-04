17eec3452aaf2959206ccca003cd6e986fe3d4ab
steellson <a.steellson@gmail.com>
2022-11-04 17:06:08 +0300

    Initial Commit

---
+++ waitumerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  waitumerApp
+//
+//  Created by Andrey Pochepaev on 04.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
